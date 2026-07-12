a3a74747fb3bca8aa5a236d641f6f9fc60d91866
Udayshankar Ravikumar <udayshankar.cto@uralstech.in>
2026-07-13 00:50:02 +0530

    Fix GPU inference.

--- Native/build.sh
+++ Native/build.sh
@@ -1,17 +1,28 @@
 cd LiteRT-LM || exit 1
 
 PLUGIN_DIR="../../UAI.LiteRTLM/Packages/com.uralstech.uai.litertlm/Runtime/Plugins"
+BUILT_SYMBOL="liblitert-lm"
 BUILD_DIR="./bazel-bin/c"
 
 build() {
     local config="$1"
+    shift
 
     bazel build                                     \
         --config="${config}"                        \
-        --define=litert_runtime_link_mode=dynamic   \
-        --define=resolve_symbols_in_exec=false      \
+        "$@"                                        \
         //c:litert-lm || return 1
 
+}
+
+patch_prebuilt_lib_android() {
+    local arch="$1"
+    local lib="$2"
+    local dst="${PLUGIN_DIR}/Android/${arch}/${lib}.so"
+
+    if ! patchelf --print-needed "${dst}" | grep -q "^${BUILT_SYMBOL}\.so$"; then
+        patchelf --add-needed "${BUILT_SYMBOL}.so" "${dst}"
+    fi
 }
 
 copy_libs() {
@@ -24,8 +35,10 @@ copy_libs() {
     local dst="${PLUGIN_DIR}/${plugin_subdir}/${arch}"
 
     mkdir -p "$dst"
-    for lib in libLiteRtTopKOpenClSampler   \
-               libLiteRtTopKWebGpuSampler; do
+    for lib in libGemmaModelConstraintProvider  \
+               libLiteRtTopKWebGpuSampler       \
+               libLiteRtWebGpuAccelerator       \
+               libwebgpu_dawn; do
 
         cp \
             "${src}/${lib}.${extension}" \
@@ -33,12 +46,13 @@ copy_libs() {
     done
 
     cp \
-        "${BUILD_DIR}/liblitert-lm.${extension}" \
-        "${dst}/liblitert-lm.${extension}"
+        "${BUILD_DIR}/${BUILT_SYMBOL}.${extension}" \
+        "${dst}/${BUILT_SYMBOL}.${extension}"
 }
 
 # Set ANDROID_NDK_HOME before running this
 # ------------------------------ Android ------------------------------
 
-build android_arm64 || exit 1
+build android_arm64 --linkopt=-Wl,-z,max-page-size=16384 || exit 1
 copy_libs android arm64 so Android
+patch_prebuilt_lib_android arm64 libLiteRtTopKWebGpuSampler
