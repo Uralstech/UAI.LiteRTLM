cfd6322879b00bee1b0908cc15209a09d65c463a
Udayshankar Ravikumar <udayshankar.cto@uralstech.in>
2026-07-13 10:31:27 +0530

    Added copyright notice.

--- Runtime/Scripts/Native/NativeAPI.cs
+++ Runtime/Scripts/Native/NativeAPI.cs
@@ -1,3 +1,17 @@
+// Copyright 2026 URAV ADVANCED LEARNING SYSTEMS PRIVATE LIMITED
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System;
 using System.Runtime.InteropServices;
 
@@ -758,13 +772,13 @@ namespace Uralstech.UAI.LiteRTLM
             /// <param name="extraContext">A JSON string representing the extra context to use.</param>
             /// <param name="optionalArgs">A pointer to the optional arguments to use.</param>
             /// <returns>
-            /// A pointer to the JSON response, or <see cref="IntPtr.Zero"/> on failure.
+            /// A pointer to the JSON Response, or <see cref="IntPtr.Zero"/> on failure.
             /// The caller is responsible for deleting the response using <see cref="JsonResponse.litert_lm_json_response_delete"/>.
             /// </returns>
             [DllImport(LibLiteRTLM, CallingConvention = CallingConvention.Cdecl)]
             public static extern IntPtr litert_lm_conversation_send_message(IntPtr conversation,
                 [MarshalAs(UnmanagedType.LPUTF8Str)] string messageJson,
-                [MarshalAs(UnmanagedType.LPUTF8Str)] string extraContext, IntPtr optionalArgs);
+                [MarshalAs(UnmanagedType.LPUTF8Str)] string? extraContext, IntPtr optionalArgs);
 
             /// <summary>
             /// Sends a message to the conversation and streams the response via a
