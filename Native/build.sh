ceaa5f9874272a90f155716d05930ded5d59c659
Udayshankar Ravikumar <udayshankar.cto@uralstech.in>
2026-07-16 10:41:07 +0530

    Added experimental iOS binaries.

--- Native/build.sh
+++ Native/build.sh
@@ -4,6 +4,10 @@ PLUGIN_DIR="../../UAI.LiteRTLM/Packages/com.uralstech.uai.litertlm/Runtime/Plugi
 BUILT_SYMBOL="liblitert-lm"
 BUILD_DIR="./bazel-bin/c"
 
+PREBUILT_LIBS_COMMON="libLiteRt libGemmaModelConstraintProvider"
+PREBUILT_LIBS_ANDROID="libLiteRtTopKOpenClSampler libLiteRtOpenClAccelerator"
+PREBUILT_LIBS_APPLE="libLiteRtTopKMetalSampler libLiteRtMetalAccelerator"
+
 build() {
     local config="$1"
     shift
@@ -25,29 +29,33 @@ patch_prebuilt_lib_android() {
     fi
 }
 
+force_copy_file() {
+    local src_file="$1"
+    local dst_file="$2"
+
+    rm -f "${dst_file}"
+    cp "${src_file}" "${dst_file}"
+}
+
 copy_libs() {
     local platform="$1"
     local arch="$2"
     local extension="$3"
     local plugin_subdir="$4"
+    local platform_libs="$5"
 
     local src="./prebuilt/${platform}_${arch}"
     local dst="${PLUGIN_DIR}/${plugin_subdir}/${arch}"
 
     mkdir -p "$dst"
-    for lib in libLiteRt                        \
-               libGemmaModelConstraintProvider  \
-               libLiteRtTopKOpenClSampler       \
-               libLiteRtOpenClAccelerator       \
-               libLiteRtTopKMetalSampler        \
-               libLiteRtMetalAccelerator; do
+    for lib in ${PREBUILT_LIBS_COMMON} ${platform_libs}; do
 
-        cp \
-            "${src}/${lib}.${extension}" \
+        force_copy_file                     \
+            "${src}/${lib}.${extension}"    \
             "${dst}/${lib}.${extension}"
     done
 
-    cp \
+    force_copy_file                                 \
         "${BUILD_DIR}/${BUILT_SYMBOL}.${extension}" \
         "${dst}/${BUILT_SYMBOL}.${extension}"
 }
@@ -56,7 +64,7 @@ copy_libs() {
 # ------------------------------ Android ------------------------------
 
 build android_arm64 --linkopt=-Wl,-z,max-page-size=16384 || exit 1
-copy_libs android arm64 so Android
+copy_libs android arm64 so Android "${PREBUILT_LIBS_ANDROID}"
 patch_prebuilt_lib_android arm64 libLiteRtTopKOpenClSampler
 
 
@@ -66,10 +74,17 @@ patch_prebuilt_lib_android arm64 libLiteRtTopKOpenClSampler
 # liblitert-lm.dylib to register it.
 
 build macos_arm64 --linkopt=-Wl,-rpath,@loader_path \
-    --linkopt="-Wl,-exported_symbol,_LiteRt*"       \
-    --linkopt="-Wl,-exported_symbol,_litert_lm_*"   \
     --define=litert_link_capi_so=true               \
     --define=litert_runtime_link_mode=dynamic       \
     --define=resolve_symbols_in_exec=false || exit 1
 
-copy_libs macos arm64 dylib macOS
+copy_libs macos arm64 dylib macOS "${PREBUILT_LIBS_APPLE}"
+
+
+# ------------------------------   iOS   ------------------------------
+
+build ios_arm64 || exit 1
+copy_libs ios arm64 dylib iOS "${PREBUILT_LIBS_APPLE}"
+
+build ios_sim_arm64 || exit 1
+copy_libs ios sim_arm64 dylib iOS "${PREBUILT_LIBS_APPLE}"
